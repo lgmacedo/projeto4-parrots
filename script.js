a9c4a42f5dbe7be518d69c717623faf6e4d1f782
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-11 13:02:43 -0300

    cartas se tornam azuis ao serem clicadas & criacao de uma array c o numero de cartas escolhidas embaralhadas

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+let numCartas = 0;
+let deckCartas = ["bobrossparrot",
+                    "explodyparrot", 
+                    "fiestaparrot", 
+                    "metalparrot", 
+                    "revertitparrot", 
+                    "tripletsparrot", 
+                    "unicornparrot"];
+let contador = 0;
+
+deckCartas = shuffleArray(deckCartas);
+
+alert(deckCartas);
+
 while(true){
-    let numCartas = prompt("Com quantas cartas deseja jogar? (insira um número par, de 4 a 14)");
+    numCartas = prompt("Com quantas cartas deseja jogar? (insira um número par, de 4 a 14)");
     if((numCartas % 2 === 0) && (numCartas >= 4) && (numCartas <= 14)){
         break;
     }
+}
+
+deckCartas = deckCartas.slice(0, numCartas/2);
+alert(deckCartas);
+deckCartas = deckCartas.concat(deckCartas);
+alert(deckCartas);
+
+deckCartas = shuffleArray(deckCartas);
+alert(deckCartas);
+
+
+while(contador < deckCartas.length){
+    document.querySelector('main').innerHTML += `<div onclick='clicouCarta(this)'><img src='./img/back.png' alt='verso-papagaio'></div>`;
+    contador++;
+}
+
+function clicouCarta(qualCarta){
+    qualCarta.style.background = "blue";
 }
