dd026e46a58f9bdca9d6b2a6e4f84ae7a8a123fc
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-11 10:43:41 -0300

    layout padrão,com background, título, sem cartas & configuração do número de cartas

---
+++ script.js
@@ -0,0 +1,6 @@
+while(true){
+    let numCartas = prompt("Com quantas cartas deseja jogar? (insira um número par, de 4 a 14)");
+    if((numCartas % 2 === 0) && (numCartas >= 4) && (numCartas <= 14)){
+        break;
+    }
+}
